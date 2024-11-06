9bf7b9c420609a1001cea27a14fc9586ee707b1c
arecouz <richardcouzens96@gmail.com>
2024-11-06 17:15:17 +0000

    added cineTrack project

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -25,7 +25,7 @@ import {
 } from '@/components/ui/table';
 import { Button } from '@/components/ui/button';
 
-const ProjectCard = ({ name, link, libraries, pitch }) => {
+const ProjectCard = ({ name, link, libraries, pitch, githubLink }) => {
   const handleClick = () => {
     return <p>testing</p>;
   };
@@ -50,15 +50,18 @@ const ProjectCard = ({ name, link, libraries, pitch }) => {
             <DialogDescription>{libraries}</DialogDescription>
 
             <div>
-              <h1>{link}</h1>
+              <h1>{name}</h1>
               <div className="p-5">
                 <Button asChild>
-                  <a href="https://couzens.fly.dev/">Go to live website</a>
+                  <a href={link} target="_blank" rel="noopener noreferrer">
+                    Go to live website
+                  </a>
                 </Button>
                 <Button
                   variant="outline"
                   title="GitHub Source Code"
                   className="fixed bottom-5 right-5 "
+                  onClick={() => window.open(githubLink, '_blank')} // talking about here
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -67,10 +70,10 @@ const ProjectCard = ({ name, link, libraries, pitch }) => {
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="icon icon-tabler icons-tabler-outline icon-tabler-brand-github"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="icon icon-tabler icons-tabler-outline icon-tabler-brand-github"
                   >
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -8,39 +8,10 @@ const Projects = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 m-8">
         <ProjectCard
           name={'CineTrack'}
-          link={'/cinetrack'}
-          pitch={'track your movies'}
+          link={'https://cine-track.fly.dev/'}
+          githubLink={'https://github.com/arecouz/cinetrack'}
+          pitch={'Search, rate, and track your movies!'}
           libraries={`react, vite, express, mongoDB, API's, user authentication`}
-        />
-        <ProjectCard
-          name={'nand2tetris'}
-          link={'https://github.com/arecouz/?'}
-          image={'nand.gif'}
-          description={`
-            Starting with the (virtual) construction of NAND gates, to build combinational and sequential logic, ultimately leading to a CPU. 
-            Then assemblers were built to translate binary code into machine code and machine code into 'Jack Code' (a high level language). 
-            An OS was then built to run Jack Code on the virtual machine. The Cookie Clicker is an example of the final program running on the system implemented.
-          `}
-        />
-        <ProjectCard
-          name={'Computer Graphics'}
-          link={'https://github.com/arecouz/?'}
-          image={'nand.gif'}
-          description={`
-            Starting with the (virtual) construction of NAND gates, to build combinational and sequential logic, ultimately leading to a CPU. 
-            Then assemblers were built to translate binary code into machine code and machine code into 'Jack Code' (a high level language). 
-            An OS was then built to run Jack Code on the virtual machine. The Cookie Clicker is an example of the final program running on the system implemented.
-          `}
-        />
-        <ProjectCard
-          name={'Repo Mobile App'}
-          link={'https://github.com/arecouz/?'}
-          image={'nand.gif'}
-          description={`
-            Starting with the (virtual) construction of NAND gates, to build combinational and sequential logic, ultimately leading to a CPU. 
-            Then assemblers were built to translate binary code into machine code and machine code into 'Jack Code' (a high level language). 
-            An OS was then built to run Jack Code on the virtual machine. The Cookie Clicker is an example of the final program running on the system implemented.
-          `}
         />
         {/* Next Project Here */}
       </div>
